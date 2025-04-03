ed8404a6f9a530df75894e5f6bb31de8c1f1341c
Timothy Chelelgo <timmybarkuch007@gmail.com>
2025-04-03 12:42:25 +0300

    -Added .env.local file on frontend for backend connection. - Finished CardPopularProducts.tsx - Resolved the backend and frontend connection

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -65,7 +65,7 @@ const Navbar = () => {
           </div>
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
-            <span className="font-semibold">Ed Roh</span>
+            <span className="font-semibold">Tim Chelelgo</span>
           </div>
         </div>
         <Link href="/settings">
